578965e242244b4bac9af965344b0135a11e467c
HeavyArms0511 <learnacademy@LEARNs-MacBook-Air.local>
2022-09-05 04:08:43 -0700

    working now?

---
+++ src/screens/contactMe/ContactMe.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './contactMe.css';
+import { navigationLinks } from '../../Helpers/navigationLinks';
+
+function createLinks () {
+    return navigationLinks.map((e, index) => (
+
+        <p key={index}><a href={e.ref}>{e.name}</a></p>
+    ))
+}
+
+
+function ContactMe() {
+  return (
+    <div className="contact-container"  id="contact">
+        <div className="contact-data-links">
+            <div>
+                <p>Springfield, Illinois</p>
+                <p>Cody Mundy</p>
+                <p>codymundy5@gmail.com</p>
+                <p><a href="https://www.linkedin.com/in/cody-mundy-b2742a98/">LinkedIn</a></p>
+                </div>
+                  
+                <div>
+                   {createLinks()}
+            </div>
+         </div>
+    </div>
+  )
+}
+
+export default ContactMe
