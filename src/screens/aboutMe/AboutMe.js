65bdc981fa4ca6afde10a18973d117996dc4859c
HeavyArms0511 <learnacademy@LEARNs-MacBook-Air.local>
2022-09-12 13:42:39 -0700

    Updated About Me

--- src/screens/aboutMe/AboutMe.js
+++ src/screens/aboutMe/AboutMe.js
@@ -11,7 +11,7 @@ function AboutMe() {
         </div>
         <div className="about_text">
             <h1>ABOUT ME</h1>
-            <p>Hello, My name is Cody Mundy.  I'm a military veteren, avid video game player, former correctional officer and train conductor turned full-stack web developer.  I have fallen in love with writing and working with code with emhpasis more on the back end.  With all the discipline and training I received from being in the military I know how to be resilient and keep working on difficult tasks.  Along with that I know how to work well in a team.  Lets work togehter and make programming a great experience!
+            <p>Hello, My name is Cody Mundy.  I'm a military veteren, avid video game player, former correctional officer and train conductor turned full-stack web developer.  I have fallen in love with writing and working with code with emhpasis more on the back end.  With all the discipline and training I received from being in the military I know how to be resilient and am accountable to handle any task given to me.  Along with that I know how to work well in a team.  Lets work togehter and make programming a great experience!
             </p>
         </div>
     </div>
