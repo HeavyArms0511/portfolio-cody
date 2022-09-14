57100080c397783315ec0fb0d5b8564975e013a9
HeavyArms0511 <learnacademy@LEARNs-MacBook-Air.local>
2022-09-13 17:17:49 -0700

    working on images

--- src/screens/projects/Projects.jsx
+++ src/screens/projects/Projects.jsx
@@ -7,7 +7,7 @@ import api from '../../images/api.png'
 
 function Projects () {
    return( <div className="project_text"><h1> Projects </h1>
-       <div className="api-container">
+       <div className="treasure-container">
        <a href="https://github.com/HeavyArms0511/wildlife-tracker-api"> <img src={api} className="api" alt="wildlife api"/></a>
         <div className='treasure-container'>
         <a href="https://heavyarms0511.github.io/"><img src={treasurehunt} className='treasure' alt='first project'/></a>
